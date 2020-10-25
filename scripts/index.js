d2678c744c4ed8e24521677583a6ba3d6dd9f1c5
HappyMarvin <vmvovan@gmail.com>
2020-10-25 16:48:25 +0300

    исправить popup input

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ let profileButtonEdit = profile.querySelector('.profile__edit');
 let profilePopupClose = profilePopup.querySelector('.profile-popup__close');
 let profileName = profile.querySelector('.profile__name');
 let profileDescription = profile.querySelector('.profile__description');
-let inputProfileName = profilePopup.querySelector('.profile-popup__name');
-let inputProfileDescription = profilePopup.querySelector('.profile-popup__description');
+let inputProfileName = profilePopup.querySelector('.profile-popup__text-input_name');
+let inputProfileDescription = profilePopup.querySelector('.profile-popup__text-input_description');
 let profilePopupForm = profilePopup.querySelector('.profile-popup__form');
 
 function openProfilePopup () {
