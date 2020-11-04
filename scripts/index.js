8df995418656a5e9610b9921b170c5d7aa67ae42
HappyMarvin <vmvovan@gmail.com>
2020-11-04 19:42:36 +0300

    Добавить разметку popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,30 @@
 let profile = document.querySelector('.profile')
-let profilePopup = document.querySelector('.profile-popup');
+let profilePopup = document.querySelector('.popup_profile');
 let profileButtonEdit = profile.querySelector('.profile__edit');
-let profilePopupClose = profilePopup.querySelector('.profile-popup__close');
+let profilePopupClose = document.querySelector('.popup__close_profile');
 let profileName = profile.querySelector('.profile__name');
 let profileDescription = profile.querySelector('.profile__description');
-let inputProfileName = profilePopup.querySelector('.profile-popup__text-input_name');
-let inputProfileDescription = profilePopup.querySelector('.profile-popup__text-input_description');
-let profilePopupForm = profilePopup.querySelector('.profile-popup__form');
+let inputProfileName = profilePopup.querySelector('.popup__text-input_name');
+let inputProfileDescription = profilePopup.querySelector('.popup__text-input_description');
+let profilePopupForm = profilePopup.querySelector('.popup__form');
 
 function openProfilePopup () {
   inputProfileName.value = profileName.textContent;
   inputProfileDescription.value = profileDescription.textContent;
-  profilePopup.classList.add('profile-popup_show');
+  profilePopup.classList.add('popup_show');
 }
 
-function closeProfilePopup () {
-  profilePopup.classList.remove('profile-popup_show');
+function closePopup () {
+  this.classList.remove('popup_show');
 }
 
 function submitProfilePopup (evt) {
   evt.preventDefault();
   profileName.textContent = inputProfileName.value;
   profileDescription.textContent = inputProfileDescription.value;
-  closeProfilePopup ();
+  closePopup.bind(profilePopup)();
 }
 
 profileButtonEdit.addEventListener('click', openProfilePopup);
-profilePopupClose.addEventListener('click', closeProfilePopup);
+profilePopupClose.addEventListener('click', closePopup.bind(profilePopup));
 profilePopupForm.addEventListener('submit', submitProfilePopup);
