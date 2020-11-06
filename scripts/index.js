adb78b405fbdc3676ef183e00dff49dcc0fca7cf
HappyMarvin <vmvovan@gmail.com>
2020-11-06 09:24:38 +0300

    добавить popup new place

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,39 @@
 let profile = document.querySelector('.profile')
 let profilePopup = document.querySelector('.popup_profile');
+const newPlacePopup = document.querySelector('.popup_new-place');
 let profileButtonEdit = profile.querySelector('.profile__edit');
+const profileButtonAdd = profile.querySelector('.profile__add');
 let profilePopupClose = document.querySelector('.popup__close_profile');
+let placePopupClose = document.querySelector('.popup__close_new-place');
 let profileName = profile.querySelector('.profile__name');
 let profileDescription = profile.querySelector('.profile__description');
 let inputProfileName = profilePopup.querySelector('.popup__text-input_name');
 let inputProfileDescription = profilePopup.querySelector('.popup__text-input_description');
 let profilePopupForm = profilePopup.querySelector('.popup__form');
 
+function openPopup(popup) {
+  popup.classList.add('popup_show');
+}
+
 function openProfilePopup () {
   inputProfileName.value = profileName.textContent;
   inputProfileDescription.value = profileDescription.textContent;
-  profilePopup.classList.add('popup_show');
+  openPopup(profilePopup);
 }
 
-function closePopup () {
-  this.classList.remove('popup_show');
+function closePopup (popup) {
+  popup.classList.remove('popup_show');
 }
 
 function submitProfilePopup (evt) {
   evt.preventDefault();
   profileName.textContent = inputProfileName.value;
   profileDescription.textContent = inputProfileDescription.value;
-  closePopup.bind(profilePopup)();
+  closePopup(profilePopup);
 }
 
 profileButtonEdit.addEventListener('click', openProfilePopup);
-profilePopupClose.addEventListener('click', closePopup.bind(profilePopup));
+profilePopupClose.addEventListener('click', () => closePopup(profilePopup));
 profilePopupForm.addEventListener('submit', submitProfilePopup);
+profileButtonAdd.addEventListener('click', () => openPopup(newPlacePopup));
+placePopupClose.addEventListener('click', () => closePopup(newPlacePopup));
